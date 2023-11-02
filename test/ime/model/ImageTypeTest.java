b0f5177641660b06591df9c9d4e8f19e00247ac2
Harikrishnan U <36120990+hkrish28@users.noreply.github.com>
2023-11-02 18:51:29 -0400

    Style update

--- test/ime/model/ImageTypeTest.java
+++ test/ime/model/ImageTypeTest.java
@@ -18,11 +18,11 @@ public class ImageTypeTest {
   }
 
   @Test
-  public void testChannelIndexForRgb(){
+  public void testChannelIndexForRgb() {
     ImageType imageType = ImageType.RGB;
-    assertEquals(0,imageType.colorChannels.indexOf(ColorChannel.RED));
-    assertEquals(1,imageType.colorChannels.indexOf(ColorChannel.GREEN));
-    assertEquals(2,imageType.colorChannels.indexOf(ColorChannel.BLUE));
+    assertEquals(0, imageType.colorChannels.indexOf(ColorChannel.RED));
+    assertEquals(1, imageType.colorChannels.indexOf(ColorChannel.GREEN));
+    assertEquals(2, imageType.colorChannels.indexOf(ColorChannel.BLUE));
   }
 
 }
