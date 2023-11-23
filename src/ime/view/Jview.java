a5ef797f6b066395c62399dfcfcd443bcd3b2f41
ashmitha <ashmithajain2810@gmail.com>
2023-11-22 21:02:59 -0500

    gui chnGEA

--- src/ime/view/Jview.java
+++ src/ime/view/Jview.java
@@ -23,7 +23,7 @@ public class Jview extends JFrame implements ActionListener, GUIView {
   private Runnable operation;
   private JPanel mainPanel;
 
-  private ButtonGroup rGroup;
+  private ButtonGroup radioGroup;
 
   private JScrollPane mainScrollPane;
 
@@ -37,23 +37,21 @@ public class Jview extends JFrame implements ActionListener, GUIView {
     setSize(800, 800);
 
     mainPanel = new JPanel();
-    //for elements to be arranged vertically within this panel
     mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
-    //scroll bars around this main panel
     mainScrollPane = new JScrollPane(mainPanel);
     add(mainScrollPane);
-// intialise the options
     initialiseOption();
+
     //dialog boxes
-    JPanel dialogBoxesPanel = new JPanel();
-    dialogBoxesPanel.setBorder(BorderFactory.createTitledBorder("Dialog boxes"));
-    dialogBoxesPanel.setLayout(new BoxLayout(dialogBoxesPanel, BoxLayout.PAGE_AXIS));
-    mainPanel.add(dialogBoxesPanel);
+    JPanel StartMenuPanel = new JPanel();
+    StartMenuPanel.setBorder(BorderFactory.createTitledBorder("Start Menu"));
+    StartMenuPanel.setLayout(new BoxLayout(StartMenuPanel, BoxLayout.PAGE_AXIS));
+    mainPanel.add(StartMenuPanel);
 
     //file open
     JPanel fileopenPanel = new JPanel();
     fileopenPanel.setLayout(new FlowLayout());
-    dialogBoxesPanel.add(fileopenPanel);
+    StartMenuPanel.add(fileopenPanel);
     JButton fileOpenButton = new JButton("Open a file");
     fileOpenButton.setActionCommand("Open file");
     fileOpenButton.addActionListener(this);
@@ -62,7 +60,7 @@ public class Jview extends JFrame implements ActionListener, GUIView {
     //file save
     JPanel filesavePanel = new JPanel();
     filesavePanel.setLayout(new FlowLayout());
-    dialogBoxesPanel.add(filesavePanel);
+    StartMenuPanel.add(filesavePanel);
     JButton fileSaveButton = new JButton("Save a file");
     fileSaveButton.setActionCommand("Save file");
     fileSaveButton.addActionListener(this);
@@ -70,7 +68,7 @@ public class Jview extends JFrame implements ActionListener, GUIView {
     // toggle button
     JPanel togglePanel = new JPanel();
     togglePanel.setLayout(new FlowLayout());
-    dialogBoxesPanel.add(togglePanel);
+    StartMenuPanel.add(togglePanel);
     JButton toggleButton = new JButton("Toggle");
     toggleButton.setActionCommand("Toggle");
     toggleButton.addActionListener(this);
@@ -78,13 +76,12 @@ public class Jview extends JFrame implements ActionListener, GUIView {
 
 // showing an image
     imagePanel = new JPanel();
-    //a border around the panel with a caption
     imagePanel.setBorder(BorderFactory.createTitledBorder("Showing an image"));
     imagePanel.setLayout(new BorderLayout());
     mainPanel.add(imagePanel);
     mainImage = new JLabel();
     JScrollPane imageScrollPane = new JScrollPane(mainImage);
-    imageScrollPane.setPreferredSize(new Dimension(256, 320));
+    imageScrollPane.setPreferredSize(new Dimension(256, 450));
     String image = "koala.jpg";
     ImageIcon imageIcon = new ImageIcon(image);
     imageIcon = new ImageIcon(image);
@@ -93,7 +90,6 @@ public class Jview extends JFrame implements ActionListener, GUIView {
 
 // showing histogram
     JPanel imagePanelHistogram = new JPanel();
-    //a border around the panel with a caption
     imagePanelHistogram.setBorder(BorderFactory.createTitledBorder("Showing histogram"));
     imagePanelHistogram.setLayout(new BorderLayout());
     mainPanel.add(imagePanelHistogram);
@@ -104,23 +100,20 @@ public class Jview extends JFrame implements ActionListener, GUIView {
     ImageIcon imageIconH = new ImageIcon(imageHistogram);
     histogramImage.setIcon(imageIconH);
     imagePanelHistogram.add(imageScrollPaneHistogram, BorderLayout.CENTER);
+
     // radio buttons
     JPanel radioPanel = new JPanel();
     radioPanel.setBorder(BorderFactory.createTitledBorder("Filter options "));
     radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.PAGE_AXIS));
-
-
     JRadioButton[] radioButtons = new JRadioButton[optionList.size()];
-    this.rGroup = new ButtonGroup();
-
+    this.radioGroup = new ButtonGroup();
     for (int i = 0; i < optionList.size(); i++) {
       radioButtons[i] = new JRadioButton(optionList.get(i).getName());
       radioButtons[i].setActionCommand(optionList.get(i).getName());
-      rGroup.add(radioButtons[i]);
+      radioGroup.add(radioButtons[i]);
       radioPanel.add(radioButtons[i]);
       radioButtons[i].addActionListener(this);
       if (optionList.get(i).isPreviewable()) {
-        // Add a preview button for each option
         JButton previewButton = new JButton("Preview");
         previewButton.setActionCommand("Preview");
         int finalI = i;
@@ -128,22 +121,9 @@ public class Jview extends JFrame implements ActionListener, GUIView {
                 previewImage(previewPercent -> features.previewOperation(optionList.get(finalI).getCommandEnum(), previewPercent)));
         radioPanel.add(previewButton);
       }
-
-      /*if (optionList.get(i).additionalInputs != null) {
-        JTextField jTextField[] = new JTextField[optionList.get(i).getAdditionalInputs().size()];
-        feildmap.put(optionList.get(i).getName(),jTextField);
-        for (int j = 0; j < optionList.get(i).getAdditionalInputs().size(); j++) {
-           jTextField[i] = new JTextField(
-              optionList.get(i).getAdditionalInputs().get(j).getName());
-
-          jTextField[i].setVisible(false);
-          jTextField[i].setPreferredSize(new Dimension(100,50));
-          radioPanel.add(inputField);
-        }
-      }*/
     }
-
     mainPanel.add(radioPanel);
+
     // apply filter button
     JPanel applyfilterPanel = new JPanel();
     applyfilterPanel.setLayout(new FlowLayout());
@@ -152,7 +132,6 @@ public class Jview extends JFrame implements ActionListener, GUIView {
     applyfilterButton.setActionCommand("Apply filter");
     applyfilterButton.addActionListener(this);
     applyfilterPanel.add(applyfilterButton);
-
   }
 
   private void previewImage(Consumer<Integer> previewer) {
@@ -190,7 +169,6 @@ public class Jview extends JFrame implements ActionListener, GUIView {
         saveFileAction();
         break;
       case "Apply filter":
-        // Implement apply filter action
         applyFilterAction();
         break;
       case "Toggle":
@@ -224,12 +202,7 @@ public class Jview extends JFrame implements ActionListener, GUIView {
         operation = () -> features.applySepia();
         break;
       case "Compression":
-        /*JTextField[] compressFeild = feildmap.get("Compression");
-        for (int i=0;i< compressFeild.length;i++){
-        compressFeild[i].setVisible(true);
-        }*/
         askForCompressprecent();
-        //operation=()->features.applyCompression(askForCompressprecent());
         break;
       case "Color Correct":
         operation = () -> features.applyColorCorrection();
@@ -247,7 +220,6 @@ public class Jview extends JFrame implements ActionListener, GUIView {
     String inputValue = JOptionPane.showInputDialog("Enter compress percent (integer):");
     try {
       int compressPercent = Integer.parseInt(inputValue);
-      //System.out.println("compress percent entered: " + compressPercent);
       operation = () -> features.applyCompression(compressPercent);
     } catch (NumberFormatException e) {
       JOptionPane.showMessageDialog(this, "Invalid input. Please enter a valid integer.", "Error",
@@ -257,13 +229,13 @@ public class Jview extends JFrame implements ActionListener, GUIView {
   }
 
   private void bmwValues() {
-    String inputValueb = JOptionPane.showInputDialog("Enter b value (integer):");
-    String inputValuem = JOptionPane.showInputDialog("Enter m value (integer):");
-    String inputValuew = JOptionPane.showInputDialog("Enter w value (integer):");
+    String inputB = JOptionPane.showInputDialog("Enter b value (integer):");
+    String inputM = JOptionPane.showInputDialog("Enter m value (integer):");
+    String inputW = JOptionPane.showInputDialog("Enter w value (integer):");
     try {
-      int b = Integer.parseInt(inputValueb);
-      int m = Integer.parseInt(inputValuem);
-      int w = Integer.parseInt(inputValuew);
+      int b = Integer.parseInt(inputB);
+      int m = Integer.parseInt(inputM);
+      int w = Integer.parseInt(inputW);
       operation = () -> features.applyLevelsAdjust(b, m, w);
     } catch (NumberFormatException e) {
       JOptionPane.showMessageDialog(this, "Invalid input. Please enter a valid integer.", "Error",
@@ -279,16 +251,15 @@ public class Jview extends JFrame implements ActionListener, GUIView {
     if (retvalue == JFileChooser.APPROVE_OPTION) {
       File f = fchooser.getSelectedFile();
       features.saveImage(f.getAbsolutePath());
-//      fileSaveDisplay.setText(f.getAbsolutePath());
     }
   }
 
   private void openFileAction() {
     final JFileChooser fchooser = new JFileChooser(".");
-    FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images", "jpg", "gif");
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG, GIF, PNG, PPM Images", "jpg", "gif","png","ppm");
     fchooser.setFileFilter(filter);
-    int retvalue = fchooser.showOpenDialog(Jview.this);
-    if (retvalue == JFileChooser.APPROVE_OPTION) {
+    int retValue = fchooser.showOpenDialog(Jview.this);
+    if (retValue == JFileChooser.APPROVE_OPTION) {
       File f = fchooser.getSelectedFile();
       features.loadImage(f.getAbsolutePath());
     }
@@ -309,7 +280,6 @@ public class Jview extends JFrame implements ActionListener, GUIView {
               JOptionPane.ERROR_MESSAGE);
       return previewAction();
     }
-//    JOptionPane.showMessageDialog(this, "Previewing: " + selectedOption);
   }
 
 
